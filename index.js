9c61f79bb4f0e3a922bd69de04117f8b259ac58d
Aswin Haridas <aswinharidas0@gmail.com>
2024-10-24 13:34:46 +0530

    commit

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ app.use(cors());
 require('dotenv').config();
 
 mongoose.connect(process.env.mongo_url).then(() => {
-    console.log('mongodb connected');
+  console.log('mongodb connected');
 });
 
 
 const router = require('./routes/blog');
 app.use('/api', router);
 
-app.use('/',(req,res) => {
-  res.sendFile(path.join(__dirname,'views','index.html'));
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 }
 
 )
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
-    console.log(`app running on port ${port}`);
+  console.log(`app running on port ${port}`);
 });
